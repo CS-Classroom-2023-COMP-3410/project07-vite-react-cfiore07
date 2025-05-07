8c222955be6ffac2250f847fd9711dc330474959
Colin Fiore <colin.fiore@du.edu>
2025-05-06 22:26:32 -0600

    add CartPage.jsx

---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,11 @@
+import ShoppingCart from './components/ShoppingCart';
+
+function CartPage() {
+    return(
+        <div style={{display:'flex', alignItems: 'center'}}>
+            <ShoppingCart />
+        </div>
+    );
+}
+
+export default CartPage;
